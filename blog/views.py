81d57d14553e41cc8daae83fe8bf3faf85d9afef
sheenaanto <sheena.anto08@gmail.com>
2025-12-19 00:40:02 +0000

    Add base HTML template and summernote image

--- blog/views.py
+++ blog/views.py
@@ -9,8 +9,10 @@ from .models import Post
 
 class PostList(generic.ListView):
     # model = Post
-    # queryset = Post.objects.all()
+    queryset = Post.objects.all()
     # queryset = Post.objects.filter(author=4)
-    queryset = Post.objects.filter(status=1)
+    # queryset = Post.objects.filter(status=1)
 
-    template_name = "post_list.html"
+    # template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6
